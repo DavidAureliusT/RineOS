e8bb885aaad28d165bc212f11fbf74da920b022d
daurelius <davad.tandjung@gmail.com>
2024-01-29 09:59:45 +0700

    Setup FE package

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,10 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+import Component from 'unplugin-vue-components/vite'
+import RadixVueResolver from 'radix-vue/resolver'
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -15,6 +18,16 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        Component({
+            dts: true,
+            resolvers: [
+                RadixVueResolver()
+        
+                // RadixVueResolver({
+                //   prefix: '' // use the prefix option to add Prefix to the imported components
+                // })
+            ],
         }),
     ],
 });
