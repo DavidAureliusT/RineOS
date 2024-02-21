2a5ea09ed0aeefb0419bf43d25b84e66222f152b
daurelius <davad.tandjung@gmail.com>
2024-02-21 09:28:51 +0700

    Click Card to Login to Crew Database

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link, router } from '@inertiajs/vue3';
+import { PhPerson } from "@phosphor-icons/vue";
 
 defineProps({
     canLogin: {
@@ -37,48 +38,43 @@ defineProps({
                 <svg width="62" height="28" viewBox="0 16 62 28" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-8 w-auto">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M37.5034 18L25.2572 45.554H32.1457L37.5034 33.3078V45.554H58.9343L61.9958 37.9001H48.2188L56.6381 18H49.7496L44.3919 31.0116V18H37.5034ZM30.4862 25.5923L33.6771 18.0001H12.4413L9.25037 25.5923H30.4862ZM8.44986 27.9586H29.6857L26.2835 35.5732H5.04768L8.44986 27.9586ZM24.6726 37.9507H3.4368L0 45.5541H21.2358L24.6726 37.9507Z" class="fill-primary"/>
                 </svg>
-                <div v-if="canLogin" class="">
-                    <Link
-                        v-if="$page.props.auth.user"
-                        :href="route('dashboard')"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                        >Dashboard</Link
-                    >
-
-                    <template v-else>
-                        <Link
-                            :href="route('login')"
-                            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >Log in</Link
-                        >
-                    </template>
-                </div>
+                <p class="px-3 text-sm text-gray-500 dark:text-gray-400 hover:underline">Help</p>
             </div>
         </div>
         
 
-        <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <div class="flex justify-center">
-                <svg width="62" height="28" viewBox="0 16 62 28" fill="none" xmlns="http://www.w3.org/2000/svg" 
-                class="h-32 w-auto">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M37.5034 18L25.2572 45.554H32.1457L37.5034 33.3078V45.554H58.9343L61.9958 37.9001H48.2188L56.6381 18H49.7496L44.3919 31.0116V18H37.5034ZM30.4862 25.5923L33.6771 18.0001H12.4413L9.25037 25.5923H30.4862ZM8.44986 27.9586H29.6857L26.2835 35.5732H5.04768L8.44986 27.9586ZM24.6726 37.9507H3.4368L0 45.5541H21.2358L24.6726 37.9507Z" class="fill-white/10"/>
-                </svg>
-            </div>
-            
-            <div class="
-            fixed inset-x-0 bottom-12
-            grid place-items-center
-            ">
-                <div class="
-                xl:max-w-6xl lg:max-w-4xl md:max-w-md sm:max-w-md
-                w-full
-                flex mt-4 items-center justify-between
-                sm:divide-x sm:divide-slate-500 lg:divide-x-0
-                text-sm text-gray-500 dark:text-gray-400
-                ">
-                    <p class="px-3">RineOS@Eka Nusantara Line</p>
-                    <p class="px-3">by davidaurelius</p>
+        <div class="xl:max-w-6xl lg:max-w-4xl md:max-w-md sm:max-w-md
+            w-full">
+            <div class="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-2">
+                
+                <div 
+                @click="router.visit(route('login'))"
+                class="col-span-1 rounded-lg border h-32 flex justify-center items-center hover:bg-primary transition-all">
+                    <div class="flex flex-col items-center text-white">
+                        <PhPerson weight="thin" size="50" class=""/>
+                        <p class="">Crew Management</p>
+                    </div>
                 </div>
+                <div class="col-span-1 rounded-lg border border-zinc-500 border-dashed h-32 flex justify-center items-center">
+                    <p class="text-white/70">Coming Soon</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="
+        fixed inset-x-0 bottom-12
+        grid place-items-center
+        ">
+            <div class="
+            xl:max-w-6xl lg:max-w-4xl md:max-w-md sm:max-w-md
+            w-full
+            flex mt-4 items-center justify-between
+            sm:divide-x sm:divide-slate-500 lg:divide-x-0
+            text-sm text-gray-500 dark:text-gray-400
+            ">
+                <p class="px-3">RineOS@Eka Nusantara Line</p>
+                <p class="px-3">by davidaurelius</p>
             </div>
         </div>
     </div>
