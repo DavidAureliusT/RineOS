daaaf895e4e39ddff851076e85214dfc76dd1cb8
daurelius <davad.tandjung@gmail.com>
2024-02-23 09:31:09 +0700

    Update InputLabel.vue

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -3,11 +3,18 @@ defineProps({
     value: {
         type: String,
     },
+    color: {
+        default: 'night'
+    }
 });
 </script>
 
 <template>
-    <label class="block font-medium text-sm text-gray-300">
+    <label :class="{
+        'block font-medium text-sm' : true,
+        'text-gray-300' : color == 'white',
+        'text-night' : color == 'night',
+        }">
         <span v-if="value">{{ value }}</span>
         <span v-else><slot /></span>
     </label>
