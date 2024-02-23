558f1d3b77d85cadaef50557843702e8c5f039cb
daurelius <davad.tandjung@gmail.com>
2024-02-23 09:30:53 +0700

    Update Crew.vue

--- resources/js/Components/Crew.vue
+++ resources/js/Components/Crew.vue
@@ -13,18 +13,19 @@ defineProps(['crew', 'url']);
  
 <template>
     <Link :href="url">
-        <div class="h-56 bg-white text-night shadow-sm transition-all border border-gray-300 
-         hover:border-accent hover:shadow-xl
-         duration-150 ease-in-out">
-            <div class="grid place-items-center h-full">
-                <div class="flex flex-col items-center">
-                    <div class="p-3 border-2 rounded-full">
-                        <PhUser size="30"/>
+        <div class="h-64 border border-transparent rounded-lg bg-white text-night shadow-sm shadow-black/80 transition-all
+         hover:shadow-inner hover:shadow-black/80
+         relative
+         ">
+            <div class="grid place-items-center justify-start px-3 h-full">
+                <div class="flex flex-col items-start">
+                    <div class="p-3 border rounded-full">
+                        <!-- <PhUser size="30" weight="thin"/> -->
+                        {{ crew.name[0] }}{{ crew.name[1] }}
                     </div>
-                    <div class="mt-3 w-52">
-                        <p class="text-lg text-center uppercase truncate">{{ crew.name }}</p>
-                        <p class="text-sm text-center">{{ crew.role }} &middot; {{ crew.vessel }}</p>
-                    </div>
+                    <p class="text-lg font-semibold absolute top-40 left-3">{{ crew.name }}</p>
+                    <p class="text-xs text-center absolute top-2 left-3">{{ crew.role }}</p>
+                    <p class="text-xs text-center absolute bottom-2 right-3">{{ crew.vessel }}</p>
                 </div>
             </div>
         </div>
