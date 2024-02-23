ebf5bbd14a2fc85b6be4041cae4a0aa1a6849fb8
daurelius <davad.tandjung@gmail.com>
2024-02-23 09:31:34 +0700

    Update PrimaryButton.vue

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,9 +1,9 @@
 <template>
     <button
         :class="{
-            'inline-flex items-center px-4 py-2 rounded-full shadow-md shadow-black/80 hover:shadow-none font-bold text-xs transition-all' : true,
-            'bg-accent text-white uppercase tracking-widest hover:shadow-inner hover:shadow-black focus:bg-accent/60 active:bg-accent/90 focus:outline-none focus:ring-2 focus:ring-accent focus:ring-offset-2' : color == 'red',
-            'bg-night/70 text-white uppercase tracking-widest hover:shadow-inner hover:shadow-black focus:bg-night/60 active:bg-night/90 focus:outline-none focus:ring-2 focus:ring-night/70 focus:ring-offset-2' : color == 'night',
+            'inline-flex items-center px-4 py-2 rounded-full shadow-md shadow-black/80 hover:shadow-inner font-bold text-xs transition-all' : true,
+            'bg-accent text-white uppercase tracking-widest hover:shadow-black focus:bg-accent/60 active:bg-accent/90 focus:outline-none focus:ring-2 focus:ring-accent focus:ring-offset-2' : color == 'red',
+            'bg-night/70 text-white uppercase tracking-widest hover:shadow-black focus:bg-night/60 active:bg-night/90 focus:outline-none focus:ring-2 focus:ring-night/70 focus:ring-offset-2' : color == 'night',
             'bg-success text-white uppercase tracking-widest hover:bg-success/70 focus:bg-success/60 active:bg-success/90 focus:outline-none focus:ring-2 focus:ring-success focus:ring-offset-2' : color == 'success'
 
     }"
